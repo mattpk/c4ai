39e16f2cc2e4b28559d8680c0789e063fe64745f
Matthew Chung <mattpkqwe@gmail.com>
2022-04-18 23:30:36 -0400

    weird

--- src/c4ai/ConnectFourAI.java
+++ src/c4ai/ConnectFourAI.java
@@ -19,10 +19,13 @@ public class ConnectFourAI {
 		while (game.winner == 0) {
 			int move;
 			if (game.isXTurn()) {
+				move = ai.suggestMove(game);
 				System.out.print("Enter a column (1-" + Game.WIDTH + "): ");
 				move = Math.min(in.nextInt()-1, Game.WIDTH-1);
 			} else {
 				move = ai.suggestMove(game);
+				System.out.print("Enter a column (1-" + Game.WIDTH + "): ");
+				move = Math.min(in.nextInt()-1, Game.WIDTH-1);
 			}
 
 			System.out.println("Placing at column " + (move+1));
