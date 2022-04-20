f1da376f6ebd3eedbc7f89f95b6d85aa977255fa
Matthew Chung <mattpkqwe@gmail.com>
2022-04-19 23:43:45 -0400

    old is better

--- src/c4ai/AI.java
+++ src/c4ai/AI.java
@@ -4,19 +4,10 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.*;
 
 public class AI {
-	private int maxDepth = 6;
+	public static int DEPTH = 6;
 	private int recursions = 0;
 	private int bestMove = -1;
-
-	public int actualTurnNum = -1;
-	
-	private List<double[]> scoredMoves;
 	private Game copy;
-
-	public AI(){}
-	public AI(int maxDepth) {
-		this.maxDepth = maxDepth;
-	}
 
 	private void debug(String s) {
 		if (false) {
@@ -37,23 +28,11 @@ public class AI {
 	public int suggestMove(Game game) {
 		copy = new Game(game);
 		bestMove = -1;
-		minimax(game, 0, 1, false, 0);
+		minimax(game, 0, 1, 0);
 		return bestMove;
 	}
 
-	public List<double[]> suggestMoves(Game game) {
-		copy = new Game(game);
-		scoredMoves = new ArrayList<>(7);
-		minimax(game, 0, 1, true, 0);
-		Collections.sort(scoredMoves,
-			Comparator.comparing(
-			(double[] m) -> 0-m[1])
-		);
-		return scoredMoves;
-	}
-
-	private double minimax(Game game, double minGuaranteedForMaximizer, double maxGuaranteedForMinimizer,
-						   boolean recordAllScores, int depth) {
+	private double minimax(Game game, double minGuaranteedForMaximizer, double maxGuaranteedForMinimizer, int depth) {
 		boolean isMaximizing = game.isXTurn();
 		// base cases
 		double salt = randInt(0, 1000) / 1000000.0;
@@ -69,32 +48,16 @@ public class AI {
 			return 0.5;
 		}
 		List<Integer> moves = game.legalMoves();
-		if (maxDepth > 4) {
-			moves = preSort(game, moves);
-		}
 
-		if (depth >= maxDepth) {
-			if (maxDepth <= 2) {
-				return (minGuaranteedForMaximizer + maxGuaranteedForMinimizer)/2;
-			}
-			if (((copy.turn > 12 && depth < maxDepth + 1)
-				|| (copy.turn > 17 && depth < maxDepth + 2)
-				|| (copy.turn > 24 && depth < maxDepth + 3)
-				|| (copy.turn > 30 && depth < maxDepth + 4)
-				|| (copy.turn > 42 && depth < maxDepth + 5))) {
-			} else {
-				return monteCarloValue(game);
-			}
+		if (depth >= DEPTH) {
+			return monteCarloValue(game);
 		}
 
 		if (isMaximizing) {
 			double max = 0.0;
 			for (Integer move : moves) {
 				game.move(move);
-				double ans = minimax(game, minGuaranteedForMaximizer, maxGuaranteedForMinimizer, false, depth+1);
-				if (recordAllScores) {
-					scoredMoves.add(new double[]{move, ans});
-				}
+				double ans = minimax(game, minGuaranteedForMaximizer, maxGuaranteedForMinimizer, depth+1);
 				if (ans > max) {
 					max = ans;
 					if (depth == 0) bestMove = move;
@@ -108,10 +71,10 @@ public class AI {
 					break;
 				}
 
+				if (depth > 0) {
 				 	// update minimum guaranteed for further searching
 					minGuaranteedForMaximizer = Math.max(max, minGuaranteedForMaximizer);
-				if (depth == 0) {
-					// is inaccurate when pruned
+				} else {
 					debug("move " + (move+1) + ": " + String.format("%.12f", ans));
 				}
 			}
@@ -120,10 +83,7 @@ public class AI {
 			double min = 1.00;
 			for (Integer move : moves) {
 				game.move(move);
-				double ans = minimax(game, minGuaranteedForMaximizer, maxGuaranteedForMinimizer, false, depth+1);
-				if (recordAllScores) {
-					scoredMoves.add(new double[]{move, ans});
-				}
+				double ans = minimax(game, minGuaranteedForMaximizer, maxGuaranteedForMinimizer, depth+1);
 				if (ans < min) {
 					min = ans;
 					if (depth == 0) bestMove = move;
@@ -136,10 +96,10 @@ public class AI {
 					break;
 				}
 
-				// update max guaranteed for further searching
-				maxGuaranteedForMinimizer = Math.min(min, maxGuaranteedForMinimizer);
-				if (depth == 0) {
-					// is inaccurate when pruned
+				if (depth > 0) {
+					// update max guaranteed for further searching
+					maxGuaranteedForMinimizer = Math.min(min, maxGuaranteedForMinimizer);
+				} else {
 					debug("move " + (move+1) + ": " + String.format("%.12f", ans));
 				}
 			}
@@ -171,16 +131,5 @@ public class AI {
 
 		game.toGame(copy);
 		return ans;
-	}
-
-	private List<Integer> preSort(Game game, List<Integer> moves) {
-		// do a 2 step minimax
-		AI ai = new AI(2);
-		List<double[]> scoredMoves = ai.suggestMoves(game);
-		List<Integer> list = new ArrayList<>(scoredMoves.size());
-		for (int i = 0; i < scoredMoves.size(); i++) {
-			list.add((int) scoredMoves.get(i)[0]);
-		}
-		return list;
 	}
 }
